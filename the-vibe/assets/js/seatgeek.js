d3778ba2a55620ddf443d4dcc29389b1dd406766
Danny W <dannywang@utexas.edu>
2023-06-08 20:25:59 -0400

    made a getEventData function; tried get events based on geoip

--- the-vibe/assets/js/seatgeek.js
+++ the-vibe/assets/js/seatgeek.js
@@ -2,15 +2,26 @@ var seatgeek_client_id = "MzQxNDQzNDF8MTY4NjEwMjgwNC4wNzc1ODk1";
 var seatgeek_client_secret =
   "1b45269a11cd2685a79a76e3c63190c3a50f086fc0ab96dbee76db60aec91e0e";
 
+function getEventData(url) {
+  fetch(url).then(function (response) {
+    if (response.ok) {
+      console.log(response);
+      response.json().then(function (data) {
+        console.log(data);
+        console.log(
+          "-----------------------------------------------------------"
+        );
+      });
+    } else {
+      alert("ERROR" + response.statusText);
+    }
+  });
+}
+
 var seatgeek_URL = `https://api.seatgeek.com/2/events?client_id=${seatgeek_client_id}&client_secret=${seatgeek_client_secret}`;
 
-fetch(seatgeek_URL).then(function (response) {
-  if (response.ok) {
-    console.log(response);
-    response.json().then(function (data) {
-      console.log(data);
-    });
-  } else {
-    alert("ERROR" + response.statusText);
-  }
-});
+getEventData(seatgeek_URL);
+
+var seatgeek_URL_geoip = `https://api.seatgeek.com/2/events?client_id=${seatgeek_client_id}&client_secret=${seatgeek_client_secret}&geoip=30024&range=30mi`;
+
+getEventData(seatgeek_URL_geoip);
