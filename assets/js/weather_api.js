d4204c47741a2ede33d53865dc7152baed052fda
Danny W <dannywang@utexas.edu>
2023-06-13 21:55:12 -0400

    refactored code; make a function for getting the weather API URL

--- assets/js/weather_api.js
+++ assets/js/weather_api.js
@@ -1,96 +1,73 @@
 const zipcodeInput = document.getElementById("zipcode");
 const button = document.getElementById("button-id");
 
-// Weather API setup: tomorrow.io
+// return the weather API URL
+function get_weatherAPI_URL(zipcode) {
+  // Weather API setup: tomorrow.io
+  const getTimelineURL = "https://api.tomorrow.io/v4/timelines";
+  const apikey = "bLebZBiU0rLuVaqnLdP5kKsF8LCL83WD";
 
-// const fetch = require("node-fetch");
-// const queryString = require("query-string");
-// const moment = require("moment");
+  // API results setup
+  let search_location = zipcode;
 
-const getTimelineURL = "https://api.tomorrow.io/v4/timelines";
-const apikey = "VaTD4KM7W4nSBR8fev6ibZwqfUxPG0I7";
+  // list the fields
+  const fields = [
+    "precipitationIntensity",
+    "precipitationType",
+    "windSpeed",
+    "windGust",
+    "windDirection",
+    "temperature",
+    "temperatureApparent",
+    "cloudCover",
+    "cloudBase",
+    "cloudCeiling",
+    "weatherCode",
+  ];
 
-let search_location = [40.758, -73.9855];
+  const units = "imperial";
+  const timesteps = ["current", "1h", "1d"];
+  const timezone = "America/New_York";
 
-//list the fields
-const fields = [
-  "precipitationIntensity",
-  "precipitationType",
-  "windSpeed",
-  "windGust",
-  "windDirection",
-  "temperature",
-  "temperatureApparent",
-  "cloudCover",
-  "cloudBase",
-  "cloudCeiling",
-  "weatherCode",
-];
+  // configure the time frame up to 6 hours back and 15 days out
+  const now = moment().local();
+  const startTime = moment().local(now).add(0, "minutes").toISOString();
+  const endTime = moment().local(now).add(4, "days").toISOString();
 
-// API results setup
-const units = "imperial";
-const timesteps = ["current", "1h", "1d"];
+  // combine the parameters
+  const paramsObj = {
+    apikey: apikey,
+    location: search_location,
+    fields: fields,
+    units: units,
+    timesteps: timesteps,
+    startTime: startTime,
+    //   endTime: endTime,
+    timezone: timezone,
+  };
 
-// configure the time frame up to 6 hours back and 15 days out
-const now = moment().local();
-const startTime = moment().local(now).add(0, "minutes").toISOString();
-const endTime = moment().local(now).add(4, "days").toISOString();
+  const searchParams = new URLSearchParams(paramsObj);
+  console.log(searchParams.toString());
 
-const timezone = "America/New_York";
-
-// request the timelines with all the query string parameters as options
-// const getTimelineParameters = queryString.stringify(
-//   {
-//     apikey,
-//     location,
-//     fields,
-//     units,
-//     timesteps,
-//     startTime,
-//     endTime,
-//     timezone,
-//   },
-//   { arrayFormat: "comma" }
-// );
-
-const paramsObj = {
-  apikey: apikey,
-  location: search_location,
-  fields: fields,
-  units: units,
-  timesteps: timesteps,
-  startTime: startTime,
-  //   endTime: endTime,
-  timezone: timezone,
-};
-const searchParams = new URLSearchParams(paramsObj);
-
-console.log(searchParams.toString());
-
-fetch(getTimelineURL + "?" + searchParams.toString(), {
-  method: "GET",
-  compress: true,
-})
-  .then((result) => result.json())
-  .then((json) => console.log(json))
-  .catch((error) => console.error("error: " + error));
-
-button.addEventListener("click", getWeather);
+  return getTimelineURL + "?" + searchParams.toString();
+}
 
 function getWeather() {
   const zipcode = zipcodeInput.value;
   console.log(zipcode);
 
-  //   fetch(
-  //     `https://api.tomorrow.io/v4/timelines?location=${zipcode}&timesteps=1d&units=imperial&apikey=VaTD4KM7W4nSBR8fev6ibZwqfUxPG0I7`
-  //   );
+  const url = get_weatherAPI_URL(zipcode);
+  console.log(url);
+
+  fetch(url, {
+    method: "GET",
+    compress: true,
+  })
+    .then((result) => result.json())
+    .then((json) => console.log(json))
+    .catch((error) => console.error("error: " + error));
+
+  zipcodeInput.value = "";
 }
 
-// const urlParams = new URLSearchParams("?type=list&page=20");
-// const myParam = urlParams.get("type");
-
-// console.log(myParam);
-
-// for (const [key, value] of urlParams) {
-//   console.log(`${key}: ${value}`);
-// }
+button.addEventListener("click", getWeather);
